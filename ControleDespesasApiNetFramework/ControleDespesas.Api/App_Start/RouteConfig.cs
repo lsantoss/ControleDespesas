705d75d3da215117be67ac7d07d7b47d2cb2be92
Lucas Santos <lucas.santos@otacom.com.br>
2020-06-07 20:54:39 -0300

    COMMIT 07/06/2020 - 4 - Configuração do Redoc e Página inicial da aplicação

--- ControleDespesasApiNetFramework/ControleDespesas.Api/App_Start/RouteConfig.cs
+++ ControleDespesasApiNetFramework/ControleDespesas.Api/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace ControleDespesas.Api
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Docs", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ControleDespesasApiNetFramework/ControleDespesas.Api/Controllers/Comum/DocsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http.Description;
+using System.Web.Mvc;
+
+namespace ControleDespesas.Api.Controllers.Comum
+{
+    [RequireHttps]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class DocsController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

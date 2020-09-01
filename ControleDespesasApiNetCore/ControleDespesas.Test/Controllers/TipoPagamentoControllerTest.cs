d03e8c6ded4de90dc4eda2ad090781268eecfc14
Lucas Santos <lucas.santos@otacom.com.br>
2020-09-01 10:10:03 -0300

    Testes da TipoPagamentoController

---
+++ ControleDespesasApiNetCore/ControleDespesas.Test/Controllers/TipoPagamentoControllerTest.cs
@@ -0,0 +1,254 @@
+﻿using ControleDespesas.Api.Controllers.ControleDespesas;
+using ControleDespesas.Api.Settings;
+using ControleDespesas.Dominio.Commands.TipoPagamento.Input;
+using ControleDespesas.Dominio.Commands.TipoPagamento.Output;
+using ControleDespesas.Dominio.Entidades;
+using ControleDespesas.Dominio.Handlers;
+using ControleDespesas.Dominio.Query.TipoPagamento;
+using ControleDespesas.Dominio.Repositorio;
+using ControleDespesas.Infra.Data.Repositorio;
+using ControleDespesas.Infra.Data.Settings;
+using ControleDespesas.Test.AppConfigurations.Factory;
+using ControleDespesas.Test.AppConfigurations.Models;
+using LSCode.Facilitador.Api.Results;
+using LSCode.Validador.ValidacoesNotificacoes;
+using LSCode.Validador.ValueObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace ControleDespesas.Test.Controllers
+{
+    public class TipoPagamentoControllerTest : DatabaseFactory
+    {
+        [SetUp]
+        public void Setup() => CriarBaseDeDadosETabelas();
+
+        [Test]
+        public void TipoPagamentoHealthCheck()
+        {
+            Mock<IOptions<SettingsAPI>> mockOptionsAPI = new Mock<IOptions<SettingsAPI>>();
+            mockOptionsAPI.SetupGet(m => m.Value).Returns(_settingsAPI);
+
+            Mock<IOptions<SettingsInfraData>> mockOptionsInfra = new Mock<IOptions<SettingsInfraData>>();
+            mockOptionsInfra.SetupGet(m => m.Value).Returns(_settingsInfraData);
+
+            ITipoPagamentoRepositorio repository = new TipoPagamentoRepositorio(mockOptionsInfra.Object);
+            TipoPagamentoHandler handler = new TipoPagamentoHandler(repository);
+
+            TipoPagamentoController controller = new TipoPagamentoController(repository, handler, mockOptionsAPI.Object);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.Request.Headers["ChaveAPI"] = _settingsAPI.ChaveAPI;
+
+            var responseJson = controller.TipoPagamentoHealthCheck().Result.ToJson();
+
+            var responseObj = JsonConvert.DeserializeObject<ApiTestResponse<ApiResponseModel<string, Notificacao>>>(responseJson);
+
+            Assert.AreEqual(200, responseObj.StatusCode);
+
+            Assert.True(responseObj.Value.Sucesso);
+            Assert.AreEqual("Sucesso", responseObj.Value.Mensagem);
+            Assert.AreEqual("API Controle de Despesas - Tipo de Pagamento OK", responseObj.Value.Dados);
+            Assert.Null(responseObj.Value.Erros);
+        }
+
+        [Test]
+        public void TipoPagamentos()
+        {
+            Mock<IOptions<SettingsAPI>> mockOptionsAPI = new Mock<IOptions<SettingsAPI>>();
+            mockOptionsAPI.SetupGet(m => m.Value).Returns(_settingsAPI);
+
+            Mock<IOptions<SettingsInfraData>> mockOptionsInfra = new Mock<IOptions<SettingsInfraData>>();
+            mockOptionsInfra.SetupGet(m => m.Value).Returns(_settingsInfraData);
+
+            ITipoPagamentoRepositorio repository = new TipoPagamentoRepositorio(mockOptionsInfra.Object);
+            TipoPagamentoHandler handler = new TipoPagamentoHandler(repository);
+
+            TipoPagamentoController controller = new TipoPagamentoController(repository, handler, mockOptionsAPI.Object);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.Request.Headers["ChaveAPI"] = _settingsAPI.ChaveAPI;
+
+            TipoPagamento tipoPagamento0 = new TipoPagamento(0, new Texto("DescriçãoTipoPagamento0", "Descrição", 250));
+            TipoPagamento tipoPagamento1 = new TipoPagamento(0, new Texto("DescriçãoTipoPagamento1", "Descrição", 250));
+            TipoPagamento tipoPagamento2 = new TipoPagamento(0, new Texto("DescriçãoTipoPagamento2", "Descrição", 250));
+
+            repository.Salvar(tipoPagamento0);
+            repository.Salvar(tipoPagamento1);
+            repository.Salvar(tipoPagamento2);
+
+            var responseJson = controller.TipoPagamentos().Result.ToJson().Replace("_id", "id");
+
+            var responseObj = JsonConvert.DeserializeObject<ApiTestResponse<ApiResponseModel<List<TipoPagamentoQueryResult>, Notificacao>>>(responseJson);
+
+            Assert.AreEqual(200, responseObj.StatusCode);
+
+            Assert.True(responseObj.Value.Sucesso);
+            Assert.AreEqual("Lista de tipos de pagamento obtida com sucesso", responseObj.Value.Mensagem);
+            Assert.Null(responseObj.Value.Erros);
+
+            Assert.AreEqual(1, responseObj.Value.Dados[0].Id);
+            Assert.AreEqual(tipoPagamento0.Descricao.ToString(), responseObj.Value.Dados[0].Descricao);
+
+            Assert.AreEqual(2, responseObj.Value.Dados[1].Id);
+            Assert.AreEqual(tipoPagamento1.Descricao.ToString(), responseObj.Value.Dados[1].Descricao);
+
+            Assert.AreEqual(3, responseObj.Value.Dados[2].Id);
+            Assert.AreEqual(tipoPagamento2.Descricao.ToString(), responseObj.Value.Dados[2].Descricao);
+        }
+
+        [Test]
+        public void TipoPagamento()
+        {
+            Mock<IOptions<SettingsAPI>> mockOptionsAPI = new Mock<IOptions<SettingsAPI>>();
+            mockOptionsAPI.SetupGet(m => m.Value).Returns(_settingsAPI);
+
+            Mock<IOptions<SettingsInfraData>> mockOptionsInfra = new Mock<IOptions<SettingsInfraData>>();
+            mockOptionsInfra.SetupGet(m => m.Value).Returns(_settingsInfraData);
+
+            ITipoPagamentoRepositorio repository = new TipoPagamentoRepositorio(mockOptionsInfra.Object);
+            TipoPagamentoHandler handler = new TipoPagamentoHandler(repository);
+
+            TipoPagamentoController controller = new TipoPagamentoController(repository, handler, mockOptionsAPI.Object);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.Request.Headers["ChaveAPI"] = _settingsAPI.ChaveAPI;
+
+            TipoPagamento tipoPagamento0 = new TipoPagamento(0, new Texto("DescriçãoTipoPagamento0", "Descrição", 250));
+            TipoPagamento tipoPagamento1 = new TipoPagamento(0, new Texto("DescriçãoTipoPagamento1", "Descrição", 250));
+            TipoPagamento tipoPagamento2 = new TipoPagamento(0, new Texto("DescriçãoTipoPagamento2", "Descrição", 250));
+
+            repository.Salvar(tipoPagamento0);
+            repository.Salvar(tipoPagamento1);
+            repository.Salvar(tipoPagamento2);
+
+            var command = new ObterTipoPagamentoPorIdCommand() { Id = 2 };
+
+            var responseJson = controller.TipoPagamento(command).Result.ToJson().Replace("_id", "id");
+
+            var responseObj = JsonConvert.DeserializeObject<ApiTestResponse<ApiResponseModel<TipoPagamentoQueryResult, Notificacao>>>(responseJson);
+
+            Assert.AreEqual(200, responseObj.StatusCode);
+
+            Assert.True(responseObj.Value.Sucesso);
+            Assert.AreEqual("Tipo de pagameto obtido com sucesso", responseObj.Value.Mensagem);
+            Assert.Null(responseObj.Value.Erros);
+
+            Assert.AreEqual(2, responseObj.Value.Dados.Id);
+            Assert.AreEqual(tipoPagamento1.Descricao.ToString(), responseObj.Value.Dados.Descricao);
+        }
+
+        [Test]
+        public void TipoPagamentoInserir()
+        {
+            Mock<IOptions<SettingsAPI>> mockOptionsAPI = new Mock<IOptions<SettingsAPI>>();
+            mockOptionsAPI.SetupGet(m => m.Value).Returns(_settingsAPI);
+
+            Mock<IOptions<SettingsInfraData>> mockOptionsInfra = new Mock<IOptions<SettingsInfraData>>();
+            mockOptionsInfra.SetupGet(m => m.Value).Returns(_settingsInfraData);
+
+            ITipoPagamentoRepositorio repository = new TipoPagamentoRepositorio(mockOptionsInfra.Object);
+            TipoPagamentoHandler handler = new TipoPagamentoHandler(repository);
+
+            TipoPagamentoController controller = new TipoPagamentoController(repository, handler, mockOptionsAPI.Object);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.Request.Headers["ChaveAPI"] = _settingsAPI.ChaveAPI;
+
+            var command = new AdicionarTipoPagamentoCommand()
+            {
+                Descricao = "DesciçãoTipoPagamento",
+            };
+
+            var responseJson = controller.TipoPagamentoInserir(command).Result.ToJson().Replace("_id", "id");
+
+            var responseObj = JsonConvert.DeserializeObject<ApiTestResponse<ApiResponseModel<AdicionarTipoPagamentoCommandOutput, Notificacao>>>(responseJson);
+
+            Assert.AreEqual(200, responseObj.StatusCode);
+
+            Assert.True(responseObj.Value.Sucesso);
+            Assert.AreEqual("Tipo Pagamento gravado com sucesso!", responseObj.Value.Mensagem);
+            Assert.Null(responseObj.Value.Erros);
+
+            Assert.AreEqual(1, responseObj.Value.Dados.Id);
+            Assert.AreEqual(command.Descricao, responseObj.Value.Dados.Descricao);
+        }
+
+        [Test]
+        public void TipoPagamentoAlterar()
+        {
+            Mock<IOptions<SettingsAPI>> mockOptionsAPI = new Mock<IOptions<SettingsAPI>>();
+            mockOptionsAPI.SetupGet(m => m.Value).Returns(_settingsAPI);
+
+            Mock<IOptions<SettingsInfraData>> mockOptionsInfra = new Mock<IOptions<SettingsInfraData>>();
+            mockOptionsInfra.SetupGet(m => m.Value).Returns(_settingsInfraData);
+
+            ITipoPagamentoRepositorio repository = new TipoPagamentoRepositorio(mockOptionsInfra.Object);
+            TipoPagamentoHandler handler = new TipoPagamentoHandler(repository);
+
+            TipoPagamentoController controller = new TipoPagamentoController(repository, handler, mockOptionsAPI.Object);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.Request.Headers["ChaveAPI"] = _settingsAPI.ChaveAPI;
+
+            TipoPagamento tipoPagamento = new TipoPagamento(0, new Texto("DescriçãoTipoPagamento", "Descrição", 250));
+            repository.Salvar(tipoPagamento);
+
+            var command = new AtualizarTipoPagamentoCommand()
+            {
+                Id = 1,
+                Descricao = "DescriçãoTipoPagamento - Editada"
+            };
+
+            var responseJson = controller.TipoPagamentoAlterar(command).Result.ToJson().Replace("_id", "id");
+
+            var responseObj = JsonConvert.DeserializeObject<ApiTestResponse<ApiResponseModel<AtualizarTipoPagamentoCommandOutput, Notificacao>>>(responseJson);
+
+            Assert.AreEqual(200, responseObj.StatusCode);
+
+            Assert.True(responseObj.Value.Sucesso);
+            Assert.AreEqual("Tipo Pagamento atualizado com sucesso!", responseObj.Value.Mensagem);
+            Assert.Null(responseObj.Value.Erros);
+
+            Assert.AreEqual(command.Id, responseObj.Value.Dados.Id);
+            Assert.AreEqual(command.Descricao, responseObj.Value.Dados.Descricao);
+        }
+
+        [Test]
+        public void TipoPagamentoExcluir()
+        {
+            Mock<IOptions<SettingsAPI>> mockOptionsAPI = new Mock<IOptions<SettingsAPI>>();
+            mockOptionsAPI.SetupGet(m => m.Value).Returns(_settingsAPI);
+
+            Mock<IOptions<SettingsInfraData>> mockOptionsInfra = new Mock<IOptions<SettingsInfraData>>();
+            mockOptionsInfra.SetupGet(m => m.Value).Returns(_settingsInfraData);
+
+            ITipoPagamentoRepositorio repository = new TipoPagamentoRepositorio(mockOptionsInfra.Object);
+            TipoPagamentoHandler handler = new TipoPagamentoHandler(repository);
+
+            TipoPagamentoController controller = new TipoPagamentoController(repository, handler, mockOptionsAPI.Object);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.Request.Headers["ChaveAPI"] = _settingsAPI.ChaveAPI;
+
+            TipoPagamento tipoPagamento = new TipoPagamento(0, new Texto("DescriçãoTipoPagamento", "Descrição", 250));
+            repository.Salvar(tipoPagamento);
+
+            var command = new ApagarTipoPagamentoCommand() { Id = 1 };
+
+            var responseJson = controller.TipoPagamentoExcluir(command).Result.ToJson().Replace("_id", "id");
+
+            var responseObj = JsonConvert.DeserializeObject<ApiTestResponse<ApiResponseModel<ApagarTipoPagamentoCommandOutput, Notificacao>>>(responseJson);
+
+            Assert.AreEqual(200, responseObj.StatusCode);
+
+            Assert.True(responseObj.Value.Sucesso);
+            Assert.AreEqual("Tipo Pagamento excluído com sucesso!", responseObj.Value.Mensagem);
+            Assert.Null(responseObj.Value.Erros);
+
+            Assert.AreEqual(command.Id, responseObj.Value.Dados.Id);
+        }
+
+        [TearDown]
+        public void TearDown() => DroparBaseDeDados();
+    }
+}

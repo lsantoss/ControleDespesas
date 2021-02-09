1f23021806167f5e88d3d0ffeb48c3d80d99dfd8
Lucas Santos <lucas.santos@otacom.com.br>
2021-02-09 15:28:19 -0300

    BaseController e HealthCheck

---
+++ ControleDespesas_ApiNetCore_Dapper/ControleDespesas.Api/Controllers/Comum/BaseController.cs
@@ -0,0 +1,39 @@
+﻿using ControleDespesas.Api.Settings;
+using ElmahCore;
+using LSCode.Facilitador.Api.Models.Results;
+using LSCode.Validador.ValidacoesNotificacoes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace ControleDespesas.Api.Controllers.Comum
+{
+    [Authorize]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    //[TypeFilter(typeof(RequestResponseApiAttribute))]
+    public class BaseController : ControllerBase
+    {
+        protected readonly string _ChaveAPI;
+
+        public BaseController(SettingsAPI settings) => _ChaveAPI = settings.ChaveAPI;
+
+        protected ActionResult<ApiResponse<string, Notificacao>> ResultHealthCheck()
+        {
+            try
+            {
+                if (Request.Headers["ChaveAPI"].ToString() != _ChaveAPI)
+                    return StatusCode(StatusCodes.Status401Unauthorized, new ApiResponse<object, Notificacao>("Acesso negado", new List<Notificacao>() { new Notificacao("Chave da API", "ChaveAPI não corresponde com a chave esperada") }));
+
+                return StatusCode(StatusCodes.Status200OK, new ApiResponse<string, Notificacao>("Sucesso", "API Controle de Despesas - OK"));
+            }
+            catch (Exception e)
+            {
+                HttpContext.RiseError(e);
+                return StatusCode(StatusCodes.Status500InternalServerError, new ApiResponse<object, Notificacao>("Erro", new List<Notificacao>() { new Notificacao("Erro", e.Message) }));
+            }
+        }
+    }
+}
---
+++ ControleDespesas_ApiNetCore_Dapper/ControleDespesas.Api/Controllers/Comum/HealthCheckController.cs
@@ -0,0 +1,31 @@
+﻿using ControleDespesas.Api.Settings;
+using LSCode.Facilitador.Api.Models.Results;
+using LSCode.Validador.ValidacoesNotificacoes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDespesas.Api.Controllers.Comum
+{
+    [ApiController]
+    public class HealthCheckController : BaseController
+    {
+        public HealthCheckController(SettingsAPI settings) : base(settings) { }
+
+        /// <summary>
+        /// Health Check
+        /// </summary>        
+        /// <remarks><h2><b>Afere a resposta deste contexto do serviço.</b></h2></remarks>
+        /// <response code="200">OK Request</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="500">Internal Server Error</response>
+        [HttpGet]
+        [Route("v1/healthCheck")]
+        [ProducesResponseType(typeof(ApiResponse<string, Notificacao>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse<string, Notificacao>), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(ApiResponse<string, Notificacao>), StatusCodes.Status500InternalServerError)]
+        public ActionResult<ApiResponse<string, Notificacao>> HealthCheck()
+        {
+            return ResultHealthCheck();
+        }        
+    }
+}

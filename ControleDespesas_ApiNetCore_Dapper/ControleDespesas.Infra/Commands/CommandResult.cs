664e0b9104a24e12f823874733afa90895e15eaa
Lucas Santos <lucas.santos@otacom.com.br>
2021-02-28 21:18:39 -0300

    CommandResult

--- ControleDespesas_ApiNetCore_Dapper/ControleDespesas.Infra/Commands/CommandResult.cs
+++ ControleDespesas_ApiNetCore_Dapper/ControleDespesas.Infra/Commands/CommandResult.cs
@@ -12,13 +12,6 @@ namespace ControleDespesas.Infra.Commands
         public object Dados { get; set; }
         public IReadOnlyCollection<Notificacao> Erros { get; set; }
 
-        /// <summary>Construtor da classe CommandResult.</summary>
-        /// <param name="statusCode">Indica o status code gerado na requisição.</param>
-        /// <param name="sucesso">Indica se a requisição foi realizada com sucesso.</param>
-        /// <param name="mensagem">Mensagem retornada da requisição.</param>
-        /// <param name="dados">Principais dados da requisição.</param>
-        /// <param name="erros">Notificações da requisição.</param>
-        /// <returns> Cria uma instância da classe CommandResult.</returns>
         public CommandResult(int statusCode, bool sucesso, string mensagem, object dados, List<Notificacao> erros)
         {
             StatusCode = statusCode;
@@ -28,12 +21,6 @@ namespace ControleDespesas.Infra.Commands
             Erros = erros;
         }
 
-        /// <summary>Construtor da classe CommandResult.</summary>
-        /// <param name="statusCode">Indica o status code gerado na requisição.</param>
-        /// <param name="mensagem">Mensagem retornada da requisição.</param>
-        /// <param name="propriedade">Propriedade que será notificada.</param>
-        /// <param name="notificacaoMensagem">Mensagem de notificação.</param>
-        /// <returns> Cria uma instância da classe CommandResult.</returns>
         public CommandResult(int statusCode, string mensagem, string propriedade, string notificacaoMensagem)
         {
             StatusCode = statusCode;
@@ -43,11 +30,6 @@ namespace ControleDespesas.Infra.Commands
             Erros = new List<Notificacao>() { new Notificacao(propriedade, notificacaoMensagem) };
         }
 
-        /// <summary>Construtor da classe CommandResult para requisições com sucesso.</summary>
-        /// <param name="statusCode">Indica o status code gerado na requisição.</param>
-        /// <param name="mensagem">Mensagem retornada da requisição.</param>
-        /// <param name="dados">Principais dados da requisição.</param>
-        /// <returns> Cria uma instância da classe CommandResult.</returns>
         public CommandResult(int statusCode, string mensagem, object dados)
         {
             StatusCode = statusCode;
@@ -57,11 +39,6 @@ namespace ControleDespesas.Infra.Commands
             Erros = null;
         }
 
-        /// <summary>Construtor da classe CommandResult para requisições sem sucesso.</summary>
-        /// <param name="statusCode">Indica o status code gerado na requisição.</param>
-        /// <param name="mensagem">Mensagem retornada da requisição.</param>
-        /// <param name="erros">Notificações da requisição.</param>
-        /// <returns> Cria uma instância da classe CommandResult.</returns>
         public CommandResult(int statusCode, string mensagem, List<Notificacao> erros)
         {
             StatusCode = statusCode;

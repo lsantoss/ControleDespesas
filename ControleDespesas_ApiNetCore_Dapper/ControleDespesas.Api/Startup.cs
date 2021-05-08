4d78b937b12482630ee04b509656b0500aa9f054
Lucas Santos <lucas.santos@otacom.com.br>
2021-05-08 00:06:17 -0300

    Melhorias swagger

--- ControleDespesas_ApiNetCore_Dapper/ControleDespesas.Api/Startup.cs
+++ ControleDespesas_ApiNetCore_Dapper/ControleDespesas.Api/Startup.cs
@@ -49,46 +49,7 @@ namespace ControleDespesas.Api
 
             #region Swagger
 
-            services.AddSwaggerGen(swagger =>
-            {
-                swagger.DescribeAllEnumsAsStrings();
-                swagger.DescribeAllParametersInCamelCase();
-                swagger.IncludeXmlComments($@"{AppDomain.CurrentDomain.BaseDirectory}\Swagger.xml");
-                swagger.OperationFilter<SwaggerSecurityFilter>();
-                swagger.OperationFilter<SwaggerNonBodyParameterFilter>();
-                swagger.OperationFilter<SwaggerClassTypeIgnoreFilter<Notificadora>>();
-                swagger.OperationFilter<SwaggerJsonIgnoreFilter>();
-                swagger.SwaggerDoc("v1", new Info
-                {
-                    Title = "Controle de Despesas",
-                    Version = "v1",
-                    Description = "WebApi do Projeto Controle de Despesas",
-                    Contact = new Contact
-                    {
-                        Name = "Lucas Santos",
-                        Email = "l_santos@hotmail.com.br",
-                        Url = "https://github.com/lsantoss"
-                    },
-                    License = new License
-                    {
-                        Name = "MIT License",
-                        Url = "https://github.com/lsantoss/ControleDespesas/blob/master/LICENSE"
-                    }
-                });
-
-                swagger.AddSecurityDefinition("Bearer", new ApiKeyScheme
-                {
-                    In = "header",
-                    Description = "Para autenticar use a palavra 'Bearer' + (um espaço entre a palavra Bearer e o Token) + 'Token'",
-                    Name = "Authorization",
-                    Type = "apiKey"
-                });
-
-                //swagger.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
-                //{
-                //    {"Bearer", new string[] { }},
-                //});
-            });
+            services.AddSwagger();
 
             #endregion
 
@@ -138,7 +99,7 @@ namespace ControleDespesas.Api
             services.AddTransient<IEmpresaRepository, EmpresaRepository>();
             services.AddTransient<ITipoPagamentoRepository, TipoPagamentoRepository>();
             services.AddTransient<IPagamentoRepository, PagamentoRepository>();
-            services.AddTransient<IUsuarioRepository, UsuarioRepository>(); 
+            services.AddTransient<IUsuarioRepository, UsuarioRepository>();
             services.AddTransient<ILogRequestResponseRepository, LogRequestResponseRepository>();
             services.AddTransient<IHealthCheckRepository, HealthCheckRepository>();
 
@@ -162,7 +123,8 @@ namespace ControleDespesas.Api
 
             #region Indented Pretty Print Formatting JSON
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(options => {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(options =>
+            {
                 options.SerializerSettings.Formatting = Formatting.Indented;
             });
 
---
+++ ControleDespesas_ApiNetCore_Dapper/ControleDespesas.Api/Swagger/AddSwaggerGen.cs
@@ -0,0 +1,56 @@
+﻿using LSCode.Validador.ValidacoesNotificacoes;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+
+namespace ControleDespesas.Api.Swagger
+{
+    public static class AddSwaggerGen
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.DescribeAllEnumsAsStrings();
+                swagger.DescribeAllParametersInCamelCase();
+                swagger.IncludeXmlComments($@"{AppDomain.CurrentDomain.BaseDirectory}\Swagger.xml");
+                swagger.OperationFilter<SwaggerSecurityFilter>();
+                swagger.OperationFilter<SwaggerNonBodyParameterFilter>();
+                swagger.OperationFilter<SwaggerClassTypeIgnoreFilter<Notificadora>>();
+                swagger.OperationFilter<SwaggerJsonIgnoreFilter>();
+                swagger.SwaggerDoc("v1", new Info
+                {
+                    Title = "Controle de Despesas",
+                    Version = "v1",
+                    Description = "WebApi do Projeto Controle de Despesas",
+                    Contact = new Contact
+                    {
+                        Name = "Lucas Santos",
+                        Email = "l_santos@hotmail.com.br",
+                        Url = "https://github.com/lsantoss"
+                    },
+                    License = new License
+                    {
+                        Name = "MIT License",
+                        Url = "https://github.com/lsantoss/ControleDespesas/blob/master/LICENSE"
+                    }
+                });
+
+                swagger.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    In = "header",
+                    Description = "Para autenticar use a palavra 'Bearer' + (um espaço entre a palavra Bearer e o Token) + 'Token'",
+                    Name = "Authorization",
+                    Type = "apiKey"
+                });
+
+                //swagger.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
+                //{
+                //    {"Bearer", new string[] { }},
+                //});
+            });
+
+            return services;
+        }
+    }
+}
